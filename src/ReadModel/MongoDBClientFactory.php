d16ec0c266048b8b78374058ad23b7e7336c9c31
Bram Devries <bram@madewithlove.be>
2016-04-11 23:36:28 +0200

    Allow more flexible dsn configuration

--- src/ReadModel/MongoDBClientFactory.php
+++ src/ReadModel/MongoDBClientFactory.php
@@ -7,16 +7,6 @@ use MongoDB\Client;
 class MongoDBClientFactory
 {
     /**
-     * @var string
-     */
-    protected $host = '127.0.0.1';
-
-    /**
-     * @var string
-     */
-    protected $port = '27017';
-
-    /**
      * @var array
      */
     protected $typeMap = [
@@ -32,27 +22,55 @@ class MongoDBClientFactory
      */
     public function create(array $config = [])
     {
-        $host = isset($config['host']) ? $config['host'] : $this->host;
-        $port = isset($config['port']) ? $config['port'] : $this->port;
+        $options = [];
         $driverOptions = isset($config['driver']) ? $config['driver'] : [
             'typeMap' => $this->typeMap,
         ];
 
+        if (isset($config['username'])) {
+            $options['username'] = $config['username'];
+        }
+
+        if (isset($config['password'])) {
+            $options['password'] = $config['password'];
+        }
+
         return new Client(
-            $this->getDsn($host, $port),
-            [],
+            $this->getDsn($config),
+            $options,
             $driverOptions
         );
     }
 
     /**
-     * @param $host
-     * @param $port
+     * @param $config
      *
+     * @see https://github.com/jenssegers/laravel-mongodb/blob/master/src/Jenssegers/Mongodb/Connection.php#L164
      * @return string
      */
-    protected function getDsn($host, $port)
+    protected function getDsn(array $config)
     {
-        return sprintf('mongodb://%s:%s', $host, $port);
+        if (isset($config['dsn'])) {
+            return $config['dsn'];
+        }
+
+        if (!isset($config['database'])) {
+            throw new \InvalidArgumentException('database not set on $config');
+        } else {
+            $database = $config['database'];
+        }
+
+        $host = isset($config['host']) ? $config['host'] : '127.0.0.1';
+
+        $hosts = is_array($host) ? $host : [$host];
+
+        foreach ($hosts as &$host) {
+            if (strpos($host, ':') === false and isset($config['port'])) {
+                $port = $config['port'];
+                $host = "$host:$port";
+            }
+        }
+
+        return "mongodb://".implode(',', $hosts)."/{$database}";
     }
 }
